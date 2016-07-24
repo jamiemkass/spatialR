54deaf8eb695ed76eed2d4dc7dd120bbe74338fd
ndimhypervol <jamie.m.kass@gmail.com>
2016-07-25 08:03:38 +0900

    rename shps script

---
+++ renameShp.R
@@ -0,0 +1,14 @@
+# Function to rename shapefile (.shp) and all associated files
+# fpath = full file path of any of the associated files (character)
+# newName = new name for the files (character)
+
+renameShp <- function(fpath, newName) {
+  dir <- dirname(fpath)
+  base <- basename(fpath)
+  fname <- strsplit(base, '[.]')[[1]][1]
+  ls <- list.files(path=dir, pattern=fname, full.names=TRUE)
+  sapply(ls, FUN=function(p){
+    file.rename(from=p, to=sub(pattern=fname,replacement=newName, p))
+  })
+  print(paste("Renamed", length(ls), "shapefiles."))
+}
